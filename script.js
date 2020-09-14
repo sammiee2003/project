f39d001add5317963808c6144759b2449e6402d2
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-09-14 12:27:10 +0200

    menu

---
+++ script.js
@@ -0,0 +1,8 @@
+function toonmenu(toon){
+    if (toon =='toon'){
+        document.getElementById("menu").style.display = 'block';
+    }
+    else if(toon == 'sluit'){
+        document.getElementById("menu").style.display = 'none';
+    }
+}
