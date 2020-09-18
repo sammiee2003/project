948ea5ba28dad354e1bc58058afc05c433ce49ec
Kimberly Hogeveen <2103405@talnet.nl>
2020-09-18 15:50:44 +0200

    connecten naar database 2

--- contact.php
+++ contact.php
@@ -52,6 +52,26 @@
 
 
     <?php
+
+
+// $host = '127.0.0.1:3306';
+// $db   = 'netland';
+// $user = 'root';
+// $pass = '';
+// $charset = 'utf8mb4';
+// $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+// $options = [
+//     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+//     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//     PDO::ATTR_EMULATE_PREPARES   => false,
+// ];
+// try {
+//     $dbh = new PDO($dsn, $user, $pass, $options);
+//     // echo("Connected to: " . $db . " on " . $host . " version: " . phpversion());
+//     echo("<br>");
+// } catch (\PDOException $e) {
+//     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+// }
     
 session_start(); 
 
