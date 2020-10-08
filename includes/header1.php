4756066d01143182b6cd9d2cb15f82a2e4b45f2c
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-10-08 12:20:32 +0200

    header

---
+++ includes/header1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">logo</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample03">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Producten</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown03">
+              <a class="dropdown-item" href="producten.php">Alle Producten</a>
+              <a class="dropdown-item" href="gewichten.php">Gewichten</a>
+              <a class="dropdown-item" href="elastieken.php">Elastieken</a>
+              <a class="dropdown-item" href="andere.php">Andere sportartikelen</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Inloggen</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-md-0">
+          <input class="form-control" type="text" placeholder="Search">
+        </form>
+      </div>
+    </nav>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
--- winkelwagen.php
+++ winkelwagen.php
@@ -1,5 +1,5 @@
 <?php 
-    include 'includes/header.php'; 
+    include 'includes/header1.php'; 
 ?>
 
 
@@ -15,128 +15,8 @@
 
 <body>
 
-<?php
-
-session_start();
-// Database connectie maken
-include('Config.php');
 
 
 
-// Kijk of er iets in de winkelwagen zit
-if(empty($_SESSION['winkelwagen']))
-{
-    echo '<p class="error">Uw winkelwagen is momenteel leeg.</p>';
-}
-// Anders
-else
-{
-    echo '<div class="wrapper">';
-        echo '<div class="row">';
-            echo '<p class="small"><b>Aantal:</b></p>';
-            echo '<p class="small"><b>Art. nr.:</b></p>';
-            echo '<p class="big"><b>Product:</b></p>';
-            echo '<p class="small"><b>Actie:</b></p>';
-            echo '<p class="small"><b>Prijs:</b></p>';
-        echo '</div>';
-    
-        // Exploden
-        $cart = explode('|', $_SESSION['winkelwagen']);
-
-        // Begin formulier
-        echo '<form action="Upd_winkelwagen.php" method="post">';
-            // Show winkelwagen
-            $i = 0;
-            foreach($cart as $products)
-            {
-                // Split
-                /*
-                $product[x] -->
-                    x == 0 -> product id
-                    x == 1 -> hoeveelheid
-                */
-                $product = explode(',', $products);
-        
-                // Get product info
-                $sql = mysql_query("SELECT * FROM producten WHERE id = '".intval($product[0])."'");
-              
-                // Als query gelukt is
-                if($sql)
-                {
-                    // Als er items zijn
-                    if(mysql_num_rows($sql) > 0)
-                    {
-                        // Alle items echoën
-                        $rec = mysql_fetch_assoc($sql);
-                        $i++;
-        
-                        // Verborgen vars
-                        echo '<input type="hidden" name="productnummer_'.$i.'" value="'.$product[0].'" />';
-                        
-                        echo '<div class="row">';
-                            // Aantal
-                            echo '<p class="small">';
-                                echo '<input type="text" class="aantal_w" name="hoeveelheid_'.$i.'" value="'.$product[1].'" size="2" maxlength="2" onKeyPress="return submitenter(this,event)" />';
-                            echo '</p>';
-                            
-                            // Artikel nummer
-                            echo '<p class="small">';
-                                if($rec['voorraad'] < $product[1])
-                                {
-                                    echo '<font style="color: #FF0000;">'.$product[0].'</font>';
-                                    $error = TRUE;
-                                }
-                                else
-                                {
-                                    echo $product[0];
-                                }
-                            echo '</p>';
-                            
-                            // titel
-                            echo '<p class="big">';
-                                echo $rec['titel'];
-                            echo '</p>';
-                            
-                            // Acties
-                            echo '<p class="small">';
-                                echo '<a href="javascript:removeItem('.$i.')">Del</a>';
-                            echo '</p>';
-                            
-                            // Prijs
-                            echo '<p class="small">';
-                                echo '&euro; '.($rec['prijs'] * $product[1]);
-                            echo '</p>';
-                        echo '</div>';
-                    }
-                    // Anders
-                    else
-                    {
-                        // Fout weergeven
-                        echo '<p class="error">Dit product is er niet meer.</p>';
-                    }
-                }
-                // Anders
-                else
-                {
-                    // Mysql error opvangen
-                    echo 'Er is een fout opgetreden in de query. <br />';
-                    echo mysql_error();
-                }
-            }
-        echo '</form>';
-        
-        if($error == TRUE)
-        {
-            echo '<p class="error">';
-                echo 'Van artikelen waarvan het artikelnummer rood is gekleurd hebben we niet voldoende op voorraad om je bestelling direct uit te kunnen leveren.';
-            echo '</p>';
-        }
-    echo '</div>';
-    
-    // Winkelwagen leeghalen & Afrekenen
-    echo '<a href="javascript:removeCart()">Winkelwagen leeghalen</a><br />';
-    echo '<a href="Afrekenen.php">Afrekenen</a></p>';
-}
-?>
 </body>
 </html>
